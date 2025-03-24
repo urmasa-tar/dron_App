48f039ec10bca4c542b011426e1e824f394fcd2d
urmasa <nik.goltsev.97@bk.ru>
2025-03-24 14:43:17 +0300

    req_first_version

---
+++ drone_backend/app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    
+    # Регистрация blueprint'ов
+    from app.routes.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+    
+    from app.routes.website import bp as website_bp
+    app.register_blueprint(website_bp)
+    
+    return app
