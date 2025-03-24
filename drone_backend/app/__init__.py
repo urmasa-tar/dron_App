ca9b47bb6ba9aee3f9f72c6737f21cad2b2c4370
urmasa <nik.goltsev.97@bk.ru>
2025-03-24 14:46:10 +0300

    Structer_final

--- drone_backend/app/__init__.py
+++ drone_backend/app/__init__.py
@@ -1,14 +1,9 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
-
-db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
-    db.init_app(app)
     
     # Регистрация blueprint'ов
     from app.routes.api import bp as api_bp
